4165b75965a819eb7179b10d1bd641a728c32efd
joaopsalescarvalho <jp0489214@gmail.com>
2024-01-28 14:30:31 -0300

    a

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -5,12 +5,12 @@ const router = require('./router');
 const app = express()
 
 app.use(express.json())
-app.use(cors(
-    {headers: [
-    {  key: "Access-Control-Allow-Origin", value: 'https://agenda-front-k8f9.onrender.com:10000' },
-    {  key: "Access-Control-Allow-Methods", value: 'OPTIONS,PATCH,DELETE,PUT' },
-]}
-))
+app.use((req, res, next) => {
+    res.header( "Access-Control-Allow-Origin", 'https://agenda-front-k8f9.onrender.com:10000' )
+    res.header("Access-Control-Allow-Methods", 'OPTIONS,PATCH,DELETE,PUT')
+    app.use(cors())
+    next()
+});
 app.use(router)
 
 module.exports = app
