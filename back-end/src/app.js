b9e8a5fa1ecb6858ebb2e45cd0405efbea9f4f1a
joaopsalescarvalho <jp0489214@gmail.com>
2024-01-27 21:16:28 -0300

    fiz ajustes

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -5,7 +5,12 @@ const router = require('./router');
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {headers: [
+    {  key: "Access-Control-Allow-Origin", value: 'https://agenda-front-k8f9.onrender.com' },
+    {  key: "Access-Control-Allow-Methods", value: 'OPTIONS,PATCH,DELETE,PUT' },
+]}
+))
 app.use(router)
 
 module.exports = app
