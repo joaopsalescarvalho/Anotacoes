ccc2edd4040f4181dc515c623f20359a997cba66
joaopsalescarvalho <jp0489214@gmail.com>
2024-02-15 15:19:40 -0300

    Update app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use((req, res, next) => {
-    res.header( "Access-Control-Allow-Origin", 'https://agenda-front-k8f9.onrender.com:10000' )
+    res.header( "Access-Control-Allow-Origin", '*' )
     res.header("Access-Control-Allow-Methods", 'OPTIONS,PATCH,DELETE,PUT')
     app.use(cors())
     next()
